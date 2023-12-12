dfc2286f2cf1ea8a0a345df61b0bfe3b9cb93589
Spencer Everett <spencerweverett@gmail.com>
2023-12-11 17:50:28 -0800

    Updated repo organizational files

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
-# TODO
+from setuptools import setup, find_packages
+
+setup(
+    name='nazara',
+    version='0.0.1',
+    description='We are legion',
+    author='Spencer Everett',
+    author_email='spencerweverett@gmail.com',
+    url='https://github.com/sweverett/nazara',
+    packages=find_packages(),
+    install_requires=['numpy', 'astropy', 'pyyaml', 'galsim'],
+)
